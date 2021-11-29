b3fc71e3aa8fae5fa60616bfa2b63089c74e2475
bbonche <bbonche@henix.fr>
2021-11-29 09:56:06 +0100

    edit mail message & send relaunch alert mail IF txt file already present

--- src/main/java/fr/ans/psc/pscload/service/emailing/EmailNature.java
+++ src/main/java/fr/ans/psc/pscload/service/emailing/EmailNature.java
@@ -2,7 +2,7 @@ package fr.ans.psc.pscload.service.emailing;
 
 public enum EmailNature {
     PROCESS_FINISHED("Le process pscload s'est terminé, le fichier %s a été généré à partir du fichier %s.", "PSCLOAD - Fin de process"),
-    PROCESS_RELAUNCHED("Les fichiers %s n'est pas cohérent avec le fichier %s. Reprise du process.", "PSCLOAD - Reprise du process");
+    PROCESS_RELAUNCHED("Le fichier %s n'est pas cohérent avec le fichier %s. Reprise du process.", "PSCLOAD - Reprise du process");
 
     public String message;
     public String subject;
